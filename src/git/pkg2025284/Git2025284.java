b8291dc60043fd25c0cae6e6a2c6b8e567ed9d4d
Reginald-Ghub <udolisahr@gmail.com>
2025-05-01 12:59:02 +0100

    second commit added student-id

--- src/git/pkg2025284/Git2025284.java
+++ src/git/pkg2025284/Git2025284.java
@@ -15,6 +15,7 @@ public class Git2025284 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Reginald 2025284");
     }
     
 }
